0c89a358d943bcd16d0b27f4caa5bcee156051a1
Nidelson Gimenez <nidelson@gmail.com>
2023-04-07 23:13:46 -0300

    add main process

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import cliProgress from "cli-progress";
+
+import { ACCOUNT_NAME, SELLER_ORIGIN, SELLER_TARGET } from "../env";
+import { SkuBinding } from "../types";
+import {
+  getSellerSkuList,
+  insertSellerSku,
+  removeSellerSku,
+  sendNotification,
+} from "./service/skuBindingService";
+// import { waitFor } from "./service/utils";
+// import { mockedSkuListOrigin } from "./service/mock";
+
+const formatTime = (date: Date) => {
+  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
+
+async function CatalogTransfer() {
+  console.group("CATALOG TRANSFER");
+
+  const startTime = new Date();
+  console.log({ start: formatTime(startTime) });
+  console.log();
+
+  const accountName = ACCOUNT_NAME;
+  const sellerOrigin = SELLER_ORIGIN;
+  const sellerTarget = SELLER_TARGET;
+
+  console.log({ accountName });
+  console.log({ sellerOrigin });
+  console.log({ sellerTarget });
+  console.log();
+
+  console.log("Getting SKU Binding list...");
+  console.log();
+
+  // const skuListOrigin = await getSellerSkuList(sellerOrigin);
+  // const skuListOrigin = mockedSkuListOrigin(sellerOrigin);
+  // const skuListOrigin: SkuBinding[] = [];
+
+  const array = await getSellerSkuList(sellerOrigin);
+
+  // filter first 10 items
+  const skuListOrigin = array.slice(0, 10);
+
+  const progressBar = new cliProgress.SingleBar({
+    forceRedraw: true,
+  });
+  progressBar.start(skuListOrigin.length, 0);
+
+  try {
+    const promises = skuListOrigin.map(async (sku: SkuBinding) => {
+      await executeTransfer(sku, sellerTarget);
+
+      progressBar.updateETA();
+      progressBar.increment();
+      // await waitFor(1000);
+
+      return sku;
+    });
+
+    await Promise.all(promises);
+  } catch (error) {
+    console.log("ERROR MAP");
+    throw error;
+  } finally {
+    progressBar.stop();
+    console.log();
+
+    const endTime = new Date();
+    console.log({ done: formatTime(endTime) });
+    // console.log({ elaptime: endTime-startTime })
+  }
+
+  console.groupEnd();
+}
+
+async function executeTransfer(sku: SkuBinding, sellerIdTarget: string) {
+  await removeSellerSku(sku);
+  await insertSellerSku(sku, sellerIdTarget);
+  await sendNotification(sellerIdTarget, sku.SellerStockKeepingUnitId);
+}
+
+// Run process
+CatalogTransfer();
