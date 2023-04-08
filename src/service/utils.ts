5f469353b33e13fe5969e3a3eb8916ca09dba21f
Nidelson Gimenez <nidelson@gmail.com>
2023-04-08 08:48:04 -0300

    add padZero helper

--- src/service/utils.ts
+++ src/service/utils.ts
@@ -1,1 +1,7 @@
 export const waitFor = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+export function padZero(value: number, size: number) {
+  let str = value.toString();
+  while (str.length < size) str = "0" + str;
+  return str;
+}
