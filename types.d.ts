8ed0ac3c8143a6c5a0e5b903bdc5d8fa53d2f998
Nidelson Gimenez <nidelson@gmail.com>
2023-04-07 23:07:21 -0300

    add types

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+export interface SkuBinding {
+  SellerStockKeepingUnitId: string; // '2094408',
+  CategoryCommissionPercentage: string | null; // null,
+  FreightCommissionPercentage: number | null; // null,
+  ProductCommissionPercentage: number | null; // null,
+  SellerId: string; // 'aquamar'
+  StockKeepingUnitId: number; // 61259,
+  IsActive: boolean; // true,
+}
